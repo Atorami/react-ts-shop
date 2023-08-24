8ed4fcabd82feda4af4cbfc6035630d893ca1762
Atorami <atoramiseo@mail.ru>
2023-08-24 15:29:54 +0200

    feat: add structure and styles for pormo, about sections

--- client/src/app/pages/MainPage.tsx
+++ client/src/app/pages/MainPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import chair from '../../components/assets/img/promo_chair.png';
 import lamp from '../../components/assets/img/promo_lamp.png';
+import about_img from '../../components/assets/img/bg/main_about_bg.png';
 
 import { ProductCard } from '../../components/ProductCard/ProductCard';
 
@@ -34,7 +35,7 @@ export default function MainPage() {
 
   return (
     <>
-      <section className="promo bg-f_sky_blue h-[764px] relative">
+      {/* <section className="promo bg-f_sky_blue h-[764px] relative">
         <div className="container mx-auto flex flex-row">
           <div className="promo__info">
             <p className="pormo__info__subtitle pt-[203px] text-f_pink">
@@ -86,7 +87,77 @@ export default function MainPage() {
       <section className="discount"></section>
       <section className="top-categories"></section>
       <section className="newslater-subscribe"></section>
-      <section className="latest-blog"></section>
+      <section className="latest-blog"></section> */}
+      <section className="promo bg-main-promo bg-center bg-cover bg-no-repeat h-[630px] relative">
+        <div className="promo-layer bg-main-promo-layer bg-cover bg-center w-full">
+          <div className="container mx-auto flex flex-col">
+            <h4 className="promo_subtitle mt-32 text-4xl italic text-o_green">
+              100% Natural Food
+            </h4>
+            <h2 className="promo_title mt-[8px] text-7xl font-bold text-o_dark_blue">
+              Choose the best
+              <br /> healthier way
+              <br /> of life
+            </h2>
+            <button className="promo_btn w-60 h-20 mt-[23px] bg-o_yellow rounded-xl text-xl font-bold">
+              Explore Now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="about h-[630px] bg-gray-100 w-full">
+        <div className="container flex flex-row justify-between h-full ">
+          <div className="about_imgb-block w-1/2 justify-self-center">
+            {<img src={about_img} alt="" className=" w-full" />}
+          </div>
+
+          <div className="about_article mt-32">
+            <h4 className="about_title text-4xl italic text-o_green">
+              About Us
+            </h4>
+            <h2 className="about_m-title text-5xl font-extrabold mt-2 text-o_dark_blue">
+              We Believe in Working <br /> Accredited Farmers
+            </h2>
+            <p className="about_descr text-lg mt-3.5">
+              Simply dummy text of the printing and typesetting industry. Lorem
+              had ceased to been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley.
+            </p>
+            <ul className="about_list mt-12">
+              <li className="about_list_item">
+                <img src="" alt="" className="about_list_item_icon" />
+                <div className="about_list_item_text">
+                  <p className="list_title text-o_dark_blue font-bold">
+                    Organic Foods Only
+                  </p>
+                  <p className="list_descr">
+                    Simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum
+                  </p>
+                </div>
+              </li>
+              <li className="about_list_item">
+                <img src="" alt="" className="about_list_item_icon" />
+                <div className="about_list_item_text">
+                  <p className="list_title text-o_dark_blue font-bold">
+                    Quality Standards
+                  </p>
+                  <p className="list_descr">
+                    Simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum
+                  </p>
+                </div>
+              </li>
+            </ul>
+            <button className="about_btn w-60 h-20 mt-[23px] bg-o_green rounded-xl text-xl font-bold text-white">
+              Shop Now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="products h-[1719px]">
+        <div className="container">.</div>
+      </section>
     </>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,18 +2,30 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    // container: {
-    //   padding: '15rem',
-    // },
+    container: {
+      padding: '15rem',
+    },
     extend: {
       fontFamily: {
-        josefin: ['Josefin Regular', 'sans-serif'],
-        lato: ['Lato Regular', 'sans-serif'],
+        // josefin: ['Josefin Regular', 'sans-serif'],
+        // lato: ['Lato Regular', 'sans-serif'],
       },
       colors: {
         f_pink: '#fb2e86',
         f_red: '#fb2448',
         f_sky_blue: '#f2f0ff',
+        o_dark_blue: '#274C5B',
+        o_green: '#7EB693',
+        o_yellow: '#EFD372',
+        o_grey: '#D4D4D4',
+        o_light_grey: '#F9F8F8',
+        o_light_green: '#EFF6F1',
+        o_black: '#525C60',
+      },
+      backgroundImage: {
+        'main-promo': 'url("../components/assets/img/bg/main_promo_bg.png")',
+        'main-promo-layer':
+          'url("../components/assets/img/bg/main_promo_layer_bg/png")',
       },
     },
   },
